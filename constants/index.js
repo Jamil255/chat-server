07b28cf55685e1f543384e16bf94c3abac1dc6a7
unknown <jamilafzal255@gmail.com>
2024-12-21 22:42:12 +0500

    update

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,8 @@
-const corsOption={
-    origin: 'http://localhost:5173',
-    credentials: true,
+const corsOption = {
+  origin: 'http://localhost:5173', // Your client app URL
+  credentials: true, // Allow cookies with cross-origin requests
+  methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PUT'], // Allow GET, POST, and OPTIONS methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow headers
 }
-  
-export{corsOption}
+
+export { corsOption }
